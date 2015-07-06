cefdb6a5437304883d351d0991c0d989e03a166d
陈阳 <chenyang@kaopubao.com>
2015-07-06 17:12:35 +0800

    complete authenticate logic and view template

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'albums#index'
+  resources 'albums'
 
+  get 'signup' => 'users#new'
+  resources 'users'
+
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
